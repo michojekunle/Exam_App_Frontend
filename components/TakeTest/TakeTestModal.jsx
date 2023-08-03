a6ecba1f4baee1c5dd845da516a13d23a4fb1eb8
AMD <michojekunle1@gmail.com>
2023-08-03 22:06:39 +0100

    done with major pages

--- components/TakeTest/TakeTestModal.jsx
+++ components/TakeTest/TakeTestModal.jsx
@@ -1,7 +1,7 @@
 const TakeTestModal = ({ handleStart }) => (
     <div className=''>
         <div className='overflow' onClick={handleStart}></div>
-        <div className="z-20 bg-white w-[480px] h-64 flex flex-col items-center justify-center absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4">
+        <div className="z-20 bg-white w-[480px] shadow-slate-400 shadow-lg  h-64 flex flex-col items-center justify-center absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4">
             <p className='text-xl text-center mb-3'>Once you click this button you are starting the test.</p>
             <button className='bg-primary py-3 px-8 mt-3 text-white text-lg rounded'>Start Test</button>
         </div>
