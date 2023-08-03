22f08b8acbc2b95e6b3b3f60d38eb996aabdbbaa
AMD <michojekunle1@gmail.com>
2023-08-03 20:40:37 +0100

    made changes

--- components/TakeTest/TakeTestModal.jsx
+++ components/TakeTest/TakeTestModal.jsx
@@ -1,6 +1,6 @@
 const TakeTestModal = ({  }) => (
     <div className="bg-white w-96 h-52  flex flex-col items-center justify-center absolute left-2/4 top-2/4 -translate-x-50 -translate-y-50">
-        <p className='text-2xl'>Once you click this button you are starting the test.</p>
+        <p className='text-2xl text-center'>Once you click this button you are starting the test.</p>
         <button className='bg-primary py-3 px-8 text-white text-lg'>Start Test</button>
     </div>
 )
