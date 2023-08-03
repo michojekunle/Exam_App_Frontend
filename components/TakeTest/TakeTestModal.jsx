8330007d61f1c0828d17acbb018e398db9d524ed
AMD <michojekunle1@gmail.com>
2023-08-03 20:32:39 +0100

    made changes

--- components/TakeTest/TakeTestModal.jsx
+++ components/TakeTest/TakeTestModal.jsx
@@ -1,9 +1,7 @@
 const TakeTestModal = ({  }) => (
-    <div className='flex items-center justify-center absolute w-full h-full bg-gray-400'>
-           <div className="bg-white w-96 h-52  flex flex-col items-center justify-center">
-            <p className=''>Once you click this button you are starting the test.</p>
-            <button className='bg-primary py-3 px-8 text-white text-lg'>Start Test</button>
-           </div>
+    <div className="bg-white w-96 h-52  flex flex-col items-center justify-center absolute left-2/4 top-2/4 -translate-x-50 -translate-y-50">
+        <p className='text-2xl'>Once you click this button you are starting the test.</p>
+        <button className='bg-primary py-3 px-8 text-white text-lg'>Start Test</button>
     </div>
 )
 
