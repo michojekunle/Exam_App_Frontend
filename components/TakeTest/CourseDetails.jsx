f18cc02890fe093f8fb07eb2a5e8ac7e7fa723a3
AMD <michojekunle1@gmail.com>
2023-08-03 09:24:52 +0100

    made changes

--- components/TakeTest/CourseDetails.jsx
+++ components/TakeTest/CourseDetails.jsx
@@ -2,14 +2,14 @@ const CourseDetails = ({ course, handleStart }) => {
   const { courseCode, courseTitle, summary } = course;
 
   return (
-    <div className='bg-white w-full p-8 relative'>
+    <div className='bg-white w-full p-8 pb-20 relative'>
       <h3 className='text-2xl mb-2'>
         <span className="font-bold uppercase">{courseCode}</span>
         {" "}
         <span>({courseTitle})</span>
       </h3>
       <p className='pl-6 text-lg'>{summary}</p>
-      <button className='bg-primary text-lg py-2 px-8 absolute right-3 bottom-8 mt-5 rounded outline-0 text-white' onClick={handleStart}>Start</button>
+      <button className='bg-primary text-lg py-2 px-8 absolute right-8 bottom-8 mt-5 rounded outline-0 text-white' onClick={handleStart}>Start</button>
     </div>
   )
 }
--- components/TakeTest/TakeTestModal.jsx
+++ components/TakeTest/TakeTestModal.jsx
@@ -1,7 +1,8 @@
 const TakeTestModal = ({  }) => (
     <div className='flex items-center justify-center absolute w-full h-full bg-gray-400'>
-           <div className="bg-white w-96 h-52  flex items-center justify-center">
-            Test Modal
+           <div className="bg-white w-96 h-52  flex flex-col items-center justify-center">
+            <p className=''>Once you click this button you are starting the test.</p>
+            <button className='bg-primary py-3 px-8 text-white text-lg'>Start Test</button>
            </div>
     </div>
 )
