0f85dd45aca472e073dff8f0ab3fa6d8167faf29
AMD <michojekunle1@gmail.com>
2023-08-09 22:14:05 +0100

    made changes

--- app/auth/layout.jsx
+++ app/auth/layout.jsx
@@ -1,7 +1,7 @@
 export default function  Layout({ children }) {
     return (
         <div className="flex w-full min-h-screen items-center justify-center">
-            <div className='w-full md:w-4/5 min-h-[500px] flex items-center'>
+            <div className='w-full md:w-4/5 min-h-[500px] flex items-center shadow-black shadow-2xl'>
                 <div className=""></div>
                 <div className="">{children}</div>
             </div>
